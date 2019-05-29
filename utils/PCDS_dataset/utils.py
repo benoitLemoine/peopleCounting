d19718e39432f4c3172a82eb78e606def6ac813c
benoitLemoine <lemo.benoit@gmail.com>
2019-05-29 13:06:28 +0200

    Improving watermark removal

--- utils/PCDS_dataset/utils.py
+++ utils/PCDS_dataset/utils.py
@@ -3,26 +3,29 @@ import numpy as np
 
 
 def removeCameraWatermark(frame):
-    lowRedBottom = np.array([0, 150, 0])
-    highRedBottom = np.array([1, 255, 255])
+    lowRedBottom = np.array([0, 100, 0])
+    highRedBottom = np.array([4, 255, 255])
 
-    lowRedMid = np.array([335, 65, 35])
-    highRedMid = np.array([345, 75, 55])
+    lowRedTop = np.array([325, 55, 35])
+    highRedTop = np.array([345, 75, 55])
 
-    lowRedTop = np.array([175, 150, 0])
-    highRedTop = np.array([180, 255, 255])
+    lowRedMid = np.array([145, 70, 0])
+    highRedMid = np.array([180, 255, 255])
 
-    frameHSV = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-    print(frameHSV[44][12])
+    rois = [frame[0:21, 0:189], frame[30:92, 178:292]]
+    processedRois = []
 
-    maskBottom = cv.inRange(frameHSV, lowRedBottom, highRedBottom)
-    maskMid = cv.inRange(frameHSV, lowRedMid, highRedMid)
-    maskTop = cv.inRange(frameHSV, lowRedTop, highRedTop)
-    mask = maskBottom + maskTop + maskMid
+    for roi in rois:
+        frameHSV = cv.cvtColor(roi, cv.COLOR_BGR2HSV)
 
-    elem = cv.getStructuringElement(cv.MORPH_CROSS, (5, 5))
-    mask = cv.dilate(mask, elem)
+        maskBottom = cv.inRange(frameHSV, lowRedBottom, highRedBottom)
+        maskMid = cv.inRange(frameHSV, lowRedMid, highRedMid)
+        maskTop = cv.inRange(frameHSV, lowRedTop, highRedTop)
+        mask = maskBottom + maskTop + maskMid
 
-    cv.rectangle(frame, (40, 8), (48, 16), (0, 185, 0))
-    # return mask
-    return cv.inpaint(frame, mask, 3, cv.INPAINT_TELEA)
+        processedRois.append(cv.inpaint(roi, mask, 3, cv.INPAINT_TELEA))
+
+    frame[0:21, 0:189] = processedRois[0]
+    frame[30:92, 178:292] = processedRois[1]
+
+    return frame
